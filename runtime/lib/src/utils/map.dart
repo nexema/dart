9f45661c28d8acfddca1bbc727ece6df158c5644
Tomás Weigenast <tomaswegenast@gmail.com>
2023-08-07 20:15:33 -0300

    change method signature

--- runtime/lib/src/utils/map.dart
+++ runtime/lib/src/utils/map.dart
@@ -1,5 +1,5 @@
 @pragma('vm:prefer-inline')
-Map<K, V> generateMap<K extends Comparable, V extends Object>(
+Map<K, V> generateMap<K extends Comparable, V>(
     int length, K Function(int index) keyGetter, V Function(int index) valueGetter) {
   final map = <K, V>{};
   for (int i = 0; i < length; i++) {
