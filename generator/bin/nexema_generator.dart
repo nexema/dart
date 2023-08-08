8b98fb814bec7cb57e31a739028c88a6f4cb68d0
Tomás Weigenast <tomaswegenast@gmail.com>
2023-08-08 18:00:08 -0300

    fix binary generator

--- generator/bin/nexema_generator.dart
+++ generator/bin/nexema_generator.dart
@@ -4,32 +4,38 @@ import 'dart:io';
 import 'package:nexema_generator/generator/generator.dart';
 import 'package:nexema_generator/models.dart';
 
-Future<void> main(List<String> args) async {
-  String? input;
-
-  // If snapshot file is specified (dev-only), parse it
-  // if(arguments["snapshot-file"] != null) {
-  //   input = File(arguments["snapshot-file"]).readAsStringSync();
-  // } else {
-  input = stdin.readLineSync(encoding: utf8);
+void main(List<String> args) {
+  String? input = stdin.readLineSync(encoding: utf8);
   if (input == null) {
     throw "empty content";
   }
-  // }
 
   // Parse definition
   var snapshot = NexemaSnapshot.fromJson(input);
 
+  final parsedArgs = Map.fromEntries(args
+      .map((e) => e.split("="))
+      .where((element) => element.length == 2)
+      .map((e) => MapEntry(e[0], e[1])));
+
   // verify arguments
-  if (args.isEmpty) {
-    _reportError("output-path argument is mandatory.");
+  final outputPath = parsedArgs["--output-path"];
+  final projectName = parsedArgs["--project-name"];
+
+  if (outputPath == null) {
+    _reportError("--output-path argument is mandatory.");
+    return;
+  }
+
+  if (projectName == null) {
+    _reportError("--project-name argument is mandatory");
     return;
   }
 
   // Create generator and execute
   var generator = Generator(
       snapshot: snapshot,
-      settings: GeneratorSettings(outputPath: args.first, projectName: args.last));
+      settings: GeneratorSettings(outputPath: outputPath, projectName: projectName));
 
   final result = generator.run();
   stdout.writeln(result.toJson());
